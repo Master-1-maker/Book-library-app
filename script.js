81c362e4ef27b193dba2737427af636b449633ec
abc <abc@example.com>
2022-10-10 13:42:38 +0400

    form values can now be appended to tbale

--- script.js
+++ script.js
@@ -12,33 +12,53 @@ const book3 = new Book("Jane Eyre", "Charlotte Brontë", "Read")
 let myLibrary = []
 
 function addBookToLibrary(...arr) {
- myLibrary.push(...arr)
+  myLibrary.push(...arr)
+ }
+ 
+ addBookToLibrary(book1)
+ addBookToLibrary(book2)
+ addBookToLibrary(book3)
+ 
+ let elBook = document.getElementById("Book");
+ let elAuthor = document.getElementById("Author");
+ let elStatus = document.getElementById("Status");
+
+function submitBook(){
+
+  if (!elBook.value.trim() || !elAuthor.value.trim())
+   return false;
+
+   const newBook = new Book(elBook.value, elAuthor.value, elStatus.value);
+   elBook.value = elAuthor.value = "";
+   elStatus.value = "Read"
+   addBookToLibrary(newBook);
+   addBookToTable(newBook);
+   return false;
 }
 
-addBookToLibrary(book1)
-addBookToLibrary(book2)
-addBookToLibrary(book3)
 
 
 
-function addBookToTable(){
+myLibrary.forEach(addBookToTable)
+function addBookToTable(b){
     let tbody = document.querySelector('tbody')
-    myLibrary.forEach(b =>{
-      let tr = document.createElement('tr')
-      let content = '<td>' + b.name + '</td><td>' + b.author + '</td>'
-      if(b.ReadOrNot == 'Read'){
-        content += '<td><button id="readbtn" class="btn rdbtn">Read</button></td>'  
-       }
-       else if(b.ReadOrNot == 'Not read'){
-         content += '<td><button id="readbtn" class="btn rdbtn">Not read</button></td>'
-       }
-       content += '<td><button class="btn delbtn" onclick="toggleDelete(this)">Delete</button></td>'
-      tr.innerHTML = content
-      tbody.appendChild(tr)
-     })
+
+    let tr = document.createElement('tr')
+  let content = '<td>' + b.name + '</td><td>' + b.author + '</td>'
+
+  if(b.ReadOrNot == 'Read'){
+    content += '<td><button id="readbtn" class="btn rdbtn">Read</button></td>'  
+   }
+   else if(b.ReadOrNot == 'Not read'){
+     content += '<td><button id="readbtn" class="btn rdbtn">Not read</button></td>'
+   }
+
+   content += '<td><button class="btn delbtn" onclick="toggleDelete(this)">Delete</button></td>'
+  tr.innerHTML = content
+  tbody.appendChild(tr)
   }
   
-  addBookToTable()
+ 
 
   function toggleDelete(o) {
     let p = o.parentNode.parentNode;
